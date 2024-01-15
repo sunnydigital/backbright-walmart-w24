bbab0fc83f75b5d0e6475669df997130829dce00
Sunny Son <sunnys2327@gmail.com>
2024-01-15 15:39:56 -0500

    Assessment 3

--- assessments/assessment-3/index.js
+++ assessments/assessment-3/index.js
@@ -4,11 +4,16 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+	alert("Form has been submitted successfully!")
 }
 
+function handleMouseOver(evt) {
+	evt.preventDefault();
+	alert("You look very nice today!")
+}
 
 let form = document.querySelector('#contact');
+const image = document.querySelector('#steak')
 
 form.addEventListener('submit', handleSubmit);
+form.addEventListener('mouseover', handleMouseOver)
