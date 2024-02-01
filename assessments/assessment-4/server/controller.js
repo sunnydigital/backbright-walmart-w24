077e15ce1bf779ddc1efad062713e2978280e0b5
Sunny Son <74517869+sunnydigital@users.noreply.github.com>
2024-01-31 21:57:52 -0500

    Updates

--- assessments/assessment-4/server/controller.js
+++ assessments/assessment-4/server/controller.js
@@ -27,39 +27,17 @@ module.exports = {
     },
 
     getPokemon : (req, res) => {
-        const pokemon = [
-            "Charizard",
-            "Pikachu",
-            "Mewtwo",
-            "Rayquaza",
-            "Groudon",
-            "Kyogre",
-            "Dialga",
-            "Palkia",
-            "Arceus",
-            "Lucario",
-            "Milotic",
-            "Togekiss",
-            "Suicune",
-            "Entei",
-            "Raikou",
-            "Roserade",
-            "Latios",
-            "Heatran",
-            "Cresselia",
-            "Cobalion",
-            "Terrakion",
-            "Landorous",
-            "Tornadus",
-            "Thunderus",
-            "Regigigas",
-            "Tornadus",
-            "Articuno"
-        ]
-        let randomIndex = Math.floor(Math.random() * pokemon.length)
-        let randomPokemon = pokemon[randomIndex]
-        
-        res.status(200).send(randomPokemon)
+        const axios = require('axios'); 
+
+        const randPokeInt = Math.floor(Math.random() * 1200)
+
+        pokeURL = `https://pokeapi.co/api/v2/pokemon/${randPokeInt}/`
+
+        axios.get(pokeURL)
+        .then(res1 => {
+            const { name } = res1.data.forms[0];
+            res.status(200).send(name)
+        })
     },
 
     getTime : (req, res) => {
