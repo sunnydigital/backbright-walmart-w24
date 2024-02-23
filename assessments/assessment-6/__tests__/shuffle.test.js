a95ea7d2199757b1202ae1e13f0ccb9b56a97c68
Sunny Son <74517869+sunnydigital@users.noreply.github.com>
2024-02-22 21:30:12 -0500

    Updates HW 6

--- assessments/assessment-6/__tests__/shuffle.test.js
+++ assessments/assessment-6/__tests__/shuffle.test.js
@@ -2,4 +2,10 @@ const shuffle = require("../src/shuffle");
 
 describe("shuffle should...", () => {
   // CODE HERE
+  test('check that shuffle returns an array', () => {
+    expect(typeof shuffle([1,2,3,4,5])).toBe('object')
+  })
+  test('check that shuffle returns an array of the same length as the argument sent in', () => {
+    expect(shuffle[1,2,3,4,5].constructor.name).toBe('Array')
+  })
 });
