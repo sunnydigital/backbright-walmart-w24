e517247f819d0aab959fe59cb5a2a47d25460773
Sunny Son <74517869+sunnydigital@users.noreply.github.com>
2024-02-24 17:24:46 -0500

    UPdates do hw 6

--- assessments/assessment-6/__tests__/duelDuo.test.js
+++ assessments/assessment-6/__tests__/duelDuo.test.js
@@ -15,4 +15,19 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+  //TO DO
+  test("check the clicking the draw button displays the div with id = \"choices\"", async () => {
+    const button = await driver.findElement(By.id('button'));
+    await button.click();
+
+    const choices = await driver.findElements(By.id('choices'));
+    expect(choices.length).toBeGreaterThan(0);
+  });
+  test("check that licking an \"add to duo\" button displays the div with id = \"player-duo\"", async () => {
+    const button = await driver.findElement(By.class('bot-btn'));
+    await button.click();
+
+    const playerDuo = await driver.findElements(By.id('player-duo'));
+    expect(playerDuo.length).toBeGreaterThan(0);
+  });
 });
--- assessments/assessment-6/__tests__/shuffle.test.js
+++ assessments/assessment-6/__tests__/shuffle.test.js
@@ -3,9 +3,9 @@ const shuffle = require("../src/shuffle");
 describe("shuffle should...", () => {
   // CODE HERE
   test('check that shuffle returns an array', () => {
-    expect(typeof shuffle([1,2,3,4,5])).toBe('object')
+    expect(shuffle([1,2,3,4,5]).constructor.name).toBe('Array')
   })
   test('check that shuffle returns an array of the same length as the argument sent in', () => {
-    expect(shuffle[1,2,3,4,5].constructor.name).toBe('Array')
+    expect(shuffle([1,2,3,4,5]).length).toBe(5)
   })
 });
